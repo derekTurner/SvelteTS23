6370e6b3fde973225bf5e8b1fdcfe067a6b09ed4
derekTurner <derek.turner@uws.ac.uk>
2023-10-16 19:15:18 +0000

    users

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,66 @@
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</svelte:head>
+          
+
+<script lang="ts">
+  import { onMount } from "svelte";
+  import axios from "axios";
+  import { Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    CardSubtitle,
+    CardText,
+    CardTitle } from "sveltestrap"; 
+
+
+const endpoint = "https://jsonplaceholder.typicode.com/users";
+
+interface Company{
+  name:string
+  catchPhrase:string;
+  bs:string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  company:Company;
+}
+
+let values: User[] = [];
+
+onMount(async function () {
+  const response = await axios.get(endpoint);
+  console.log(response.data);
+  values = response.data;
+});
+
+</script>
+
+{#each values as item}               
+<Card color="success">
+  <CardHeader>
+    <CardTitle>{item.name}</CardTitle>
+  </CardHeader>
+  <CardBody>
+    <CardSubtitle>{item.email}</CardSubtitle>
+    <CardText>
+      {item.company.name}
+    </CardText>
+  </CardBody>
+  <CardFooter>    
+      {item.company.catchPhrase}
+  </CardFooter>
+</Card>
+<br/>
+{/each}
+
+
+<card >
+	{#each values as item}
+                  <option value={item.id}>{item.name}</option>
+                {/each}
+</card>
