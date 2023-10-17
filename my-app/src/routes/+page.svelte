5103d67b2d4d04dfc9f4a6c067d775ec076eddbd
derekTurner <derek.turner@uws.ac.uk>
2023-10-17 01:17:59 +0100

    Update +page.svelte

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -2,7 +2,6 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </svelte:head>
           
-
 <script lang="ts">
   import { onMount } from "svelte";
   import axios from "axios";
